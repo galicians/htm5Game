2c8a3f25cc8454d29c70ffa15f47572dd88ad831
galicians <galicians@gmail.com>
2015-01-08 20:44:28 +0000

    main menu added

---
+++ Readme.md
@@ -0,0 +1,7 @@
+##HTML 5: game development.
+
+What a better way to learn HTML5 that through game development.
+
+Keep tune on github/galicians for next release.
+
+As always, any doubt, suggestion, demand or complain please email me to galicians@gmail.com
---
+++ js/game.js
@@ -0,0 +1,15 @@
+var game = {
+    init: function(){
+
+        $('.gamelayer').hide();
+        $('#gamestartscreen').show();
+
+        game.canvas = $('#gamecanvas')[0];
+        game.context = game.canvas.getContext('2d');
+    }
+}
+
+$(window).load(function() {
+    game.init();
+});
+
