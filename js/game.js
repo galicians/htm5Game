64195677a6c367b09db0856d9bf61e0c15ede0ba
galicians <galicians@gmail.com>
2015-01-09 12:20:04 +0000

    loader object added, now levels.load

--- js/game.js
+++ js/game.js
@@ -1,15 +1,126 @@
-var game = {
-    init: function(){
-
-        $('.gamelayer').hide();
-        $('#gamestartscreen').show();
-
-        game.canvas = $('#gamecanvas')[0];
-        game.context = game.canvas.getContext('2d');
-    }
-}
-
 $(window).load(function() {
     game.init();
 });
 
+//before the loader can be used, we need to call the loader.init() method from inside game.init()
+//son the loade loader is initialized when the game is getting initialized.
+
+var game = {
+    init: function(){
+        levels.init();
+        loader.init();
+
+        // Hide all game layers and display the start screen
+        $('.gamelayer').hide();
+        $('#gamestartscreen').show();
+        //Get handler for game canvas and context
+        game.canvas = $('#gamecanvas')[0];
+        game.context = game.canvas.getContext('2d');
+    },
+    showLevelScreen:function(){
+        $('.gamelayer').hide();
+        $('#levelselectscreen').show('slow');
+    }
+}
+
+
+
+var levels = {
+    data:[
+        {
+            foreground:'desert-foreground',
+            background:'clouds-background',
+            entities:[]
+        },
+        {
+            foreground:'desert-foreground',
+            background:'clouds-background',
+            entities:[]
+        }
+    ],
+    init:function(){
+        var html="";
+        for(var i = 0; i < levels.data.length; i++) {
+            var level = levels.data[i];
+            html += '<input type="button" value="'+(i+1)+'">';
+        };
+        $('#levelselectscreen').html(html);
+
+        $('#levelselectscreen input').click(function(){
+            levels.load(this.value-1);
+            $('#levelselectscreen').hide();
+        });
+    },
+    load:function(number){
+
+        game.currentLevel={ number: number, hero:[]};
+        game.score = 0;
+        $('#score').html('Score: ' + game.score);
+        var level = levels.data[number];
+        //load the background, foreground, and slingshot images
+    }
+}
+
+var loader = {
+    loaded:true,
+    loadedCount:0, //Assets that ave been loaded so far
+    totalCount:0,   //Total number of assets that need to be loaded
+
+    //An init() method that detects the supported audio file format and saves it.
+    init:function(){
+
+        var mp3Support,oggSupport;
+        var audio = document.createElement('audio');
+        if (audio.canPlayType) {
+            mp3Support = "" != audio.canPlayType('audio/mpeg');
+            oggSupport= "" != audio.canPlayType('audio/ogg;  codecs="vorbis');
+        } else {
+            mp3Support=false;
+            mp3Support=false;
+        }
+        // Check for ogg, then mp3, and finally set soundFileExtn to undefined
+        loader.soundFileExtn = oggSupport?".ogg":mp3Support?".mp3":undefined;  
+
+    },
+    // Two methods for loading images and audio files—loadImage() and loadSound(). Both
+    // methods increment the totalCount variable and show the loading screen when invoked.
+    loadImage:function(url){
+        this.totalCount++;
+        this.loaded=false;
+        $('#loadingscreen').show();
+        var image = new Image();
+        image.src = url;
+        image.onload = loader.itemLoaded;
+        return image;
+    },
+    soundFileExtn:".ogg",
+    loadSound:function(url){
+        this.totalCount++;
+        this.loaded=false;
+        $('#loadingscreen').show();
+        var audio = new Audio();
+        audio.src = url + loader.soundFileExtn;
+        audio.addEventListener("canplaythrough", loader.itemLoaded, false);
+        return audio;
+    },
+    // An itemLoaded() method that is invoked each time an asset finishes loading. 
+    // This method updates the loaded count and the loading message. Once all the assets are loaded,
+    // the loading screen is hidden and an optional loader.onload() method is called (if defined). 
+    // This lets us assign a callback function to call once the images are loaded.
+    // 
+    // Using a callback method makes it easy for us to wait while the images are loading and start the game 
+    // once all images have loaded
+
+    itemLoaded:function(){
+        loader.loadedCount++;
+        $('#loadingmessage').html('Loaded ' + loader.loadedCount + ' of ' + loader.totalCount);
+        if (loader.loadedCount === loader.totalCount){
+            loader.loaded = true;
+            $('#loadingscreen').hide();
+            if(loader.onload){
+                loader.onload();
+                loader.onload=undefined;
+            }
+        }
+    }
+}
